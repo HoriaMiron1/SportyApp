d4ac2243ad1d83854e4225e0ee95a671a6d0da57
Horia <horiamiron7@gmail.com>
2023-05-25 20:41:55 +0300

    HomeScreen modified

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>This is the Home Screen</Text>
+      <Text>This</Text>
     </View>
   );
 }
